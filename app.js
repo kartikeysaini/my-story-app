d15b1d675cbf1efece070dd7e7a24e0cc2b15f59
unknown <kartikey_saini@yahoo.com>
2019-05-24 12:31:37 +0530

    handled auth

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const passport = require("passport");
+//passport config
+require("./config/passport")(passport);
+
+//local routes
+const auth = require("./routes/auth");
 const app = express();
+
+app.use("/auth", auth);
 
 app.get("/", (req, res) => {
   res.send("welcome");
---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI:
+    "mongodb://kartikey:kartikey1997@ds123124.mlab.com:23124/story-books-dev-prod",
+  googleClientID:
+    "213654558708-jdpbhnft4j73edebp9vkl6362gaaeklt.apps.googleusercontent.com",
+  googleClientSecret: "CIhgMmg_r3rUL17TBFBSmWQ1"
+};
